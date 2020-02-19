564baaf7bf543af44e33442c8dadc70cf68121e2
wraikny <writtenknown@gmail.com>
2020-02-20 00:48:01 +0900

    init

---
+++ README.md
@@ -0,0 +1,24 @@
+# fable-practice
+
+## Setup
+```sh
+dotnet tool restore
+dotnet paket restore
+dotnet paket install
+dotnet build src/FablePractice
+
+npm install
+```
+
+## Build
+```sh
+npm build
+```
+
+## Run
+```sh
+npm start
+```
+
+## Copyrights
+- [fable2-samples](https://github.com/fable-compiler/fable2-samples): Copyright(c) 2018 Fable, [MIT License](https://github.com/fable-compiler/fable2-samples/blob/master/LICENSE)
---
+++ src/FablePractice/App.fs
@@ -0,0 +1,42 @@
+﻿module App
+
+open Fable.Core.JsInterop
+open Fable.Import
+
+let window = Browser.Dom.window
+
+let mutable myCanvas : Browser.Types.HTMLCanvasElement = unbox window.document.getElementById "myCanvas"
+
+let ctx = myCanvas.getContext_2d()
+
+let w = myCanvas.width
+let h = myCanvas.height
+let steps = 20
+let squareSize = 20
+
+let gridWidth = float (steps * squareSize) 
+
+myCanvas.width <- gridWidth
+myCanvas.height <- gridWidth
+
+printfn "%i" steps
+
+[0..steps]
+|> Seq.iter (fun x ->
+  let v = float ((x) * squareSize) 
+  ctx.moveTo(v, 0.)
+  ctx.lineTo(v, gridWidth)
+  ctx.moveTo(0., v)
+  ctx.lineTo(gridWidth, v)
+) 
+
+ctx.strokeStyle <- !^"#ddd"
+
+// draw our grid
+ctx.stroke() 
+
+// write Fable
+ctx.textAlign <- "center"
+ctx.fillText("Fable on Canvas", gridWidth * 0.5, gridWidth * 0.5)
+
+printfn "done!"
